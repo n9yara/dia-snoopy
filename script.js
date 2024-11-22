ee99f290973a6ff1125825dd88b481f00c0b0587
n9yara <zdelucuaffg@gmail.com>
2024-11-21 22:17:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 4
+    var hora = data.getHours()
     msg.innerHTML = `agora são ${hora} horas`
     
      if(hora >= 6 && hora < 12){
@@ -14,12 +14,10 @@ function carregar(){
     }else if(hora >= 18 && hora <= 23){
         img.src = 'noite.png'
         document.body.style.background = '#4979af'
-        
-
+   
     }else{
         img.src = 'madrugada.gif'
         document.body.style.background = '#5d378e'
-
     }
         
 
