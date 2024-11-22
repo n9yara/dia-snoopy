4972ceda301425c7976e10c6d3e5d36d564acb95
n9yara <zdelucuaffg@gmail.com>
2024-11-21 22:05:30 -0300

    madrugada

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora = 4
     msg.innerHTML = `agora são ${hora} horas`
-    if (hora >= 0 && hora < 12){
+    
+     if(hora >= 6 && hora < 12){
         img.src = 'dia.png'
         document.body.style.background = '#89d0fd'
     }else if(hora >= 12 && hora < 18){
         img.src = 'tarde.png'
         document.body.style.background = '#efcb51'
-    }else{
+    }else if(hora >= 18 && hora <= 23){
         img.src = 'noite.png'
         document.body.style.background = '#4979af'
+        
+
+    }else{
+        img.src = 'madrugada.gif'
+        document.body.style.background = '#5d378e'
 
     }
-}
+        
+
+     }
+
+    
+
